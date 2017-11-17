baf5ecb9d5cdb387e2be48d0e688c0444a472801
James Prior <jep200404@columbus.rr.com>
2017-11-16 20:09:42 -0500

    tests/test_euler_013.py: Create first trivial test.

---
+++ tests/test_euler_013.py
@@ -0,0 +1,27 @@
+import pytest
+
+from euler_013 import first_ten_digits
+
+number_to_first_ten_digits = {
+    1234567890: 1234567890,
+}
+@pytest.mark.parametrize('number, expected_number', number_to_first_ten_digits.items())
+def test_known_number_returns_expected(number, expected_number):
+    assert expected_number == first_ten_digits(number)
+
+# bad_values_to_expected_error = {
+#     -1: ValueError,
+#     0: ValueError,
+#     1: ValueError,
+#     'hello': ValueError,
+# }
+# @pytest.mark.parametrize('bad_value, expected_error', bad_values_to_expected_error.items())
+# def test_known_value_returns_expected_error(bad_value, expected_error):
+#     # Rendundant tests show alternate syntax.
+# 
+#     with pytest.raises(expected_error):
+#         largest_factor(bad_value)
+# 
+#     # older syntax works with 2.4
+#     pytest.raises(expected_error, "largest_factor(bad_value)")
+# 
