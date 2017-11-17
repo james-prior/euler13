dfb7f2500abbd751fbd5ad577f51e83b0ef70f90
Travis Risner <deeppunster@gmail.com>
2017-11-16 21:33:37 -0500

    test_euler_013.py - added test for failing input numbers

--- tests/test_euler_013.py
+++ tests/test_euler_013.py
@@ -12,6 +12,16 @@ number_to_first_ten_digits = {
 def test_known_number_returns_expected(number, expected_number):
     assert expected_number == first_ten_digits(number)
 
+bad_values_to_expected_error = {
+    'hello': ValueError,
+}
+@pytest.mark.parametrize('bad_value, expected_error', bad_values_to_expected_error.items())
+def test_known_value_returns_expected_error(bad_value, expected_error):
+    # Rendundant tests show alternate syntax.
+
+    with pytest.raises(expected_error):
+        first_ten_digits(bad_value)
+
 # bad_values_to_expected_error = {
 #     -1: ValueError,
 #     0: ValueError,
@@ -21,10 +31,10 @@ def test_known_number_returns_expected(number, expected_number):
 # @pytest.mark.parametrize('bad_value, expected_error', bad_values_to_expected_error.items())
 # def test_known_value_returns_expected_error(bad_value, expected_error):
 #     # Rendundant tests show alternate syntax.
-# 
+#
 #     with pytest.raises(expected_error):
 #         largest_factor(bad_value)
-# 
+#
 #     # older syntax works with 2.4
 #     pytest.raises(expected_error, "largest_factor(bad_value)")
-# 
+#
