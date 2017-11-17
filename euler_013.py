83e3c2c76b122a0d3d501b5633c5edea8d4ba029
Travis Risner <deeppunster@gmail.com>
2017-11-16 21:07:27 -0500

    euler_013.py - added real code to extract the first 10 digits

--- euler_013.py
+++ euler_013.py
@@ -3,7 +3,11 @@ euler_13.py -
 """
 
 def first_ten_digits(x):
-    if x == 1234567890:
-        return 1234567890
-    if x == 544543254252554325425254325254324254:
-        return 5445432542
+    """
+    Extract the first ten digits from input.
+    :param x: input (too long) number
+    :return: first 10 digits of the number
+    """
+    nbr_string = str(x)
+    first_digits = int(nbr_string[:10])
+    return first_digits
